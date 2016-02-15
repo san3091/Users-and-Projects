571e51dc75026ebd0871ed026387a807ba465f08
san3091 <san3091@gmail.com>
2016-02-15 14:51:36 -0500

    This time for real. Scaffold for many to many relationship practice (using through)

---
+++ app/models/project.rb
@@ -0,0 +1,4 @@
+class Project < ActiveRecord::Base
+  has_many :memberships
+  has_many :users, through: :memberships
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  has_many :memberships
+  has_many :projects, through: :memberships
+end
